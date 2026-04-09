7257f977b45040e63cb2fb90f91c40cdbfd9bd82
Oleksandr Stolyarov <stolyarov_396@icloud.com>
2026-04-09 07:09:40 +0200

    Fixed imports of Prisma in text-servis

--- backend/text-service/src/prisma/prisma.service.ts
+++ backend/text-service/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../src/generated/prisma';
+import { PrismaClient } from '../generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
